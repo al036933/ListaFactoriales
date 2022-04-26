826e53a6e6c916ebadee577cbbbb02d9d7a18c0e
Cristian García Hernández <al036933@ieslaencanta.com>
2022-04-26 12:45:39 +0200

    Haciendo lista de factoriales

---
+++ src/es/cristiann/Main.java
@@ -0,0 +1,17 @@
+package es.cristiann;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int factorial = 1;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Dame un número mayor que : ");
+            factorial = factorial * i;
+        }
+
+        System.out.println(factorial);
+    }
+}
