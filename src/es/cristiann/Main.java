c5e83e2573207ee74e8d490abbf499d82e911663
Cristian García Hernández <al036933@ieslaencanta.com>
2022-04-27 09:50:37 +0200

    Lista de factoriales haciendo

--- src/es/cristiann/Main.java
+++ src/es/cristiann/Main.java
@@ -1,13 +1,18 @@
 package es.cristiann;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	    int factorial = 1;
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Dame un número mayor que 0: ");
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Dame un número mayor que 0 : " + factorial);
-            factorial *= i;
+           System.out.println();
+           factorial *= i;
+
         }
 
         System.out.println(factorial);
